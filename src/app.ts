8361b710af4f51d032bbedd1cd581d8c4d76713b
Lucas Henrique Torresin da Costa <82097583+Lunatic02@users.noreply.github.com>
2023-08-14 19:29:54 -0300

    Hashing the password

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
 import fastify from 'fastify'
-import {z} from 'zod'
-import {prisma} from './lib/prisma'
-import { request } from 'http'
-import { register } from './http/controllers/register'
+import { appRoutes } from './http/routes'
+
 
 export const app = fastify()
 
+app.register(appRoutes)
 
