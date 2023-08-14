3d0b20ef76d24ecd08d651121a748d2dbc15069b
Lucas Henrique Torresin da Costa <82097583+Lunatic02@users.noreply.github.com>
2023-08-14 20:26:48 -0300

    D - Inversão de dependencias

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -2,7 +2,8 @@ import { z } from "zod"
 import { prisma } from "../../lib/prisma"
 import { hash } from 'bcryptjs'
 import { FastifyReply, FastifyRequest } from "fastify"
-import { registerUseCase } from "../../use-cases/register"
+import { RegisterUseCase } from "../../use-cases/register"
+import { PrismaUsersRepository } from "../../repositories/prisma-users-repository"
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -14,7 +15,12 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(request.body)
 
   try {
-    await registerUseCase({ name, email, password })
+    const usersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    await registerUseCase.execute({
+      name, email, password
+    })
   } catch (err) {
     return reply.status(409).send({
       message: 'E-mail already in use'
