4973ed5ccdc7b4d616d1775a7687c57433484551
Lucas Henrique Torresin da Costa <82097583+Lunatic02@users.noreply.github.com>
2023-08-22 21:15:31 -0300

    Historico use case

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -3,4 +3,5 @@ import { Prisma, checkIn } from "@prisma/client";
 export interface CheckInsRepository {
   create(data: Prisma.checkInUncheckedCreateInput): Promise<checkIn>
   finByUserIdOnDate(userId: string, date: Date): Promise<checkIn | null>
+  findManyByUserId(userId: string, page: number): Promise<checkIn[]>
 }
