eecc88f34133f917745b32b59fadb4115aa641a1
Lucas Henrique Torresin da Costa <lucas_hcosta@live.com>
2023-08-25 19:04:15 -0300

    repositorios do prisma

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -3,6 +3,17 @@ import { prisma } from "../../lib/prisma";
 import { UsersRepository } from "../users-repository";
 
 export class PrismaUsersRepository implements UsersRepository {
+  
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      }
+    })
+
+    return user
+  }
+  
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: {
@@ -12,6 +23,7 @@ export class PrismaUsersRepository implements UsersRepository {
 
     return user
   }
+
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
       data,
