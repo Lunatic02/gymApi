0fad34e1453c4cf61d9a258223c8549b9c824a89
Lucas Henrique Torresin da Costa <82097583+Lunatic02@users.noreply.github.com>
2023-08-27 10:45:15 -0300

    Controller de perfil

--- src/use-cases/factories/make-get-user-profile-use-case.ts
+++ src/use-cases/factories/make-get-user-profile-use-case.ts
@@ -1,8 +1,7 @@
-import { PrismaUsersRepository } from "../../repositories/prisma/prisma-users-repository";
-import { AuthenticateUseCase } from "../authenticate";
-import { GetUserProfileUseCase } from "../get-user-profile";
+import { PrismaUsersRepository } from '../../repositories/prisma/prisma-users-repository'
+import { GetUserProfileUseCase } from '../get-user-profile'
 
-export function makeGetUserProfile() {
+export function makeGetUserProfileUseCase() {
   const usersRepository = new PrismaUsersRepository()
   const useCase = new GetUserProfileUseCase(usersRepository)
 
