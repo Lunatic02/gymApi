44353c814bf45203b31ccb52f4a7b0c2148c2b0a
Lucas Henrique Torresin da Costa <82097583+Lunatic02@users.noreply.github.com>
2023-08-27 10:28:17 -0300

    Implementando JWT no Fastify

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import fastify from 'fastify'
 import { appRoutes } from './http/routes'
 import { ZodError } from 'zod'
+import fastifyJwt from '@fastify/jwt'
+import { register } from './http/controllers/register'
 
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: 'pocopianao'
+})
 
 app.register(appRoutes)
 app.setErrorHandler((error, _, reply) => {
