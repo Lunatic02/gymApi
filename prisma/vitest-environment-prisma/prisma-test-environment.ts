82671216c8212ed726368dcf1593017da78f5125
Lucas Henrique Torresin da Costa <lucas_hcosta@live.com>
2023-08-30 18:58:54 -0300

    e2e e package

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,31 @@
-import { Environment } from 'vitest'
+import 'dotenv/config'
 
-export default <Environment><unknown>{
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
+import { Environment } from 'vitest'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+function generateDatabaseURL(databaseName: string) {
+  const sqliteFilePath = `./${databaseName}.sqlite` // SQLite file will be stored in the current directory
+  return `file:${sqliteFilePath}`
+}
+
+export default <Environment>{
   name: 'prisma',
   async setup() {
-    console.log('Setup')
+    const databaseName = `api_solid_test_${randomUUID().replace(/-/g, '_')}`
+    const databaseURL = generateDatabaseURL(databaseName)
+
+    process.env.DATABASE_URL = databaseURL
+
+    execSync('npx prisma generate') // Generate Prisma client for SQLite
 
     return {
       async teardown() {
-        console.log('Teardown')
+
+        await prisma.$disconnect()
       },
     }
   },
